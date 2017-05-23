c1f9ab6479670657c830d1e7e5a1ce9127cc9f7e
Jody O'Neill <jody.o.neill@gmail.com>
2017-05-23 15:22:10 +0200

    sorted som spelling errors

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -1,12 +1,12 @@
 var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
 
-// Connection URL för databasen
+// Connection URL for the database.
 function getDB(){
     return 'mongodb://212.85.88.103:27017/project';
 }
 
-// Fuction for iterating over key values and nesteld key values.
+// Fuction for iterating over key values and nested key values.
 function getKeyValue(result){
     var x = result;
     var str = "";
@@ -27,29 +27,28 @@ function getKeyValue(result){
 }
 
 
-// In this function we connect to "employee" ang get the document
+// fetching employee data.
 function getEmployee(){
     MongoClient.connect(getDB(), function(err, db) {
         if (err) throw err;
         var query = { name : /^M/ };
         db.collection("employee").find({}).toArray(function(err, result) {
-        if (err) throw err;
-        //for every position in the document we go trough every post.
-        for (var i = 0; i < result.length; i++) {
+            if (err) throw err;
+            for (var i = 0; i < result.length; i++) {
             console.log(getKeyValue(result[i]));
-        }
+            }
         db.close();
         });
     });
 }
 
+// fetching branch data.
 function getBranch(){
     MongoClient.connect(getDB(), function(err, db) {
         console.log("");
         if (err) throw err;
         db.collection("branch").find({}).toArray(function(err, result) {
             if (err) throw err;
-            //for every position in the document we go trough every post.
             for (var i = 0; i < result.length; i++) {
                 console.log(getKeyValue(result[i]));
             }
@@ -58,13 +57,13 @@ function getBranch(){
     });
 }
 
+// fetching memberclub data:
 function getMeberclub(){
     MongoClient.connect(getDB(), function(err, db) {
         console.log("memberclub");
         if (err) throw err;
         db.collection("memberclub").find({}).toArray(function(err, result) {
             if (err) throw err;
-            //for every position in the document we go trough every post.
             for (var i = 0; i < result.length; i++) {
                 console.log(getKeyValue(result[i]));
             }
