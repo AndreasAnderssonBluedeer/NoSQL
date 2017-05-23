f74c34d234bad53473406b2aee6eeb885c6a3f48
Jody O'Neill <jody.o.neill@gmail.com>
2017-05-23 22:03:15 +0200

    minor changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,69 @@
+function onLaod(){
+    var formBeans = document.getElementById("beans_order");
+    formBeans.addEventListener("submit", function(event) {
+        event.preventDefault();
+        var beans = document.getElementsByName("beans");
+        var beanAmount = document.getElementById('amountBeans').value
+        for(var i = 0; i < beans.length; i++) {
+            if(beans[i].checked == true) {
+                bean = beans[i].value;
+            }
+        }
+    }
+
+    var formDrink = document.getElementById("drink_order");
+    formBeans.addEventListener("submit", function(event) {
+        event.preventDefault();
+        var beans = document.getElementsByName("drinks");
+        var beanAmount = document.getElementById('amountBeans').value
+        for(var i = 0; i < beans.length; i++) {
+            if(beans[i].checked == true) {
+                bean = beans[i].value;
+            }
+        }
+    }
+
+
+  var elements = document.getElementById("apply-for-pet").elements;
+  if (this.elements.firstname.value.length<=50) {
+    isCorrectString+= "Firstname: "+this.elements.firstname.value+"\n";
+  }else{
+    boo = false;
+    notOkString += "Firstname name is too long\n";
+  }
+  if (this.elements.lastname.value.length<=50) {
+    isCorrectString+= "Lastname: "+this.elements.lastname.value+"\n";
+  }else{
+    boo = false;
+    notOkString += "Lastname name is too long\n";
+  }
+  if (!isNaN(this.elements.age.value)) {
+    isCorrectString+= "Age: "+this.elements.age.value+"\n";
+  }else{
+    boo = false;
+    notOkString += "Age is not a number\n";
+  }
+  if (this.elements.email.value.length<=50) {
+    isCorrectString+= "Email: "+this.elements.email.value+"\n";
+  }else{
+    boo = false;
+    notOkString += "Email is too long\n";
+  }
+  if(boo === false){
+    alert(notOkString);
+  }else{
+    isCorrectString+= "pet "+ selectedPet;
+    var r = confirm(isCorrectString);
+      if (r === true) {
+        event.target.submit();
+      }
+  }
+});
+}
+
+
+
+
 var MongoClient = require('mongodb').MongoClient
     , assert = require('assert');
 
